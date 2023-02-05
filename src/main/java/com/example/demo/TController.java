4a0b6241ea4bda8ce1a560ce673f5c3b33db6bb8
themapisto <themapisto@naver.com>
2023-02-05 23:30:29 +0900

    're'

--- src/main/java/com/example/demo/TController.java
+++ src/main/java/com/example/demo/TController.java
@@ -6,12 +6,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class TController {
 
     @Autowired
     private TService tService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
 
 
     @GetMapping("/hello-mvc")
@@ -20,6 +26,26 @@ public class TController {
         model.addAttribute("data","Hello this is koo blog");
         model.addAttribute("list",tService.MemberList(1L));
 
+
+        return "index";
+    }
+
+
+    @GetMapping("/add")
+    public String addForm(Model model){
+        model.addAttribute("member", new Member());
+        return "addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@ModelAttribute Member member, RedirectAttributes redirectAttributes) {
+
+        System.out.println(member.getId());
+        System.out.println(member.getUsername());
+
+        Member savedMember = memberRepository.save(member);
+
+        redirectAttributes.addAttribute("id", savedMember.getId());
 
         return "index";
     }
